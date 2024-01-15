464608dc43861ffc6a2df983314ed450a4aecff5
Mina <beroundou@student.21-school.ru>
2024-01-15 16:51:28 +0300

    first commit

--- Cat_test/main.c
+++ Cat_test/main.c
@@ -4,17 +4,23 @@
 #include "parser.h"
 
 int main(int argc, char **argv) {
-    flags conf = {0};
+    config conf = {0};
     char *short_options = "beEnstTv";
     struct option long_options[] = {{"number-nonblank", no_argument, NULL, 'b'},
                                     {"number", no_argument, NULL, 'n'},
                                     {"squeeze-blank", no_argument, NULL, 's'}};
+    
+    int error = parse_params(argc, argv, short_options, long_options, &conf);
+
+    if (error) {
+        return 1;
+    }
     return 0;
 }
 
-void cat(char *filename) {                                            // Coding function itself
-    FILE *file = fopen(filename, "r");                                // Opening and reading a file
-    if (file == NULL) {                                               // Starting checking for wrong input
-        perror("n/a");                                                // Prints error
-        return;                                                       // Exits function
-    }
+// void cat(char *filename) {                                            // Coding function itself
+//     FILE *file = fopen(filename, "r");                                // Opening and reading a file
+//     if (file == NULL) {                                               // Starting checking for wrong input
+//         perror("n/a");                                                // Prints error
+//         return;                                                       // Exits function
+//     }
--- Cat_test/parser.h
+++ Cat_test/parser.h
@@ -1,5 +1,5 @@
-#ifndef PARSE_H
-#define PARSE_H
+#ifndef PARSER_H
+#define PARSER_H
 
 //Объявляем структуру флагов для дальнейшего использования//
 typedef struct {
@@ -9,10 +9,11 @@ typedef struct {
   int s;
   int t;
   int v;
-} flags;
+} config;
 
 //Объявляем фунцкию для парсинга//
 int parse_params(int argc, char **argv, char *short_options,
-                 struct option long_options[], flags *conf);
+                 struct option long_options[], config *conf);
+
 
 #endif
