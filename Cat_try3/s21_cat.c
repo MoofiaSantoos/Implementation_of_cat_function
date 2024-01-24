50c237f4eacf9adc2277f937030ae7da3aa0e623
Mina <beroundou@student.21-school.ru>
2024-01-24 20:51:51 +0300

    almost finished cat

--- Cat_try3/s21_cat.c
+++ Cat_try3/s21_cat.c
@@ -1,32 +1,178 @@
 #include "s21_cat.h"
 
-
-
-int main(int argc, char **argv) {
-    cat_programm(argc, argv[1]);
+int main(int argc, char **argv)
+{
+    flags flags = CatReadFlags(argc, argv);
+    cat_programm(argc, argv[2], flags);
 }
 
+flags CatReadFlags(int argc, char *argv[])
+{
+    struct option LongOptions[] = {
+        {"number-nonblank", 0, NULL, 'b'},
+        {"number", 0, NULL, 'n'},
+        {"squeeze-blank", 0, NULL, 's'},
+        {NULL, 0, NULL, 0}};
+    int currentFlag = getopt_long(argc, argv, "bevEnstT", LongOptions, NULL);
+    flags flags = {0};
+    while (currentFlag != -1)
+    {
+        switch (currentFlag)
+        {
+            break;
+        case 'b':
+            flags.b = 1;
+            break;
+        case 'e':
+            flags.e = 1;
+        case 'v':
+            flags.v = 1;
+            break;
+        case 'E':
+            flags.e = 1;
+            break;
+        case 'n':
+            flags.n = 1;
+            break;
+        case 's':
+            flags.s = 1;
+            break;
+        case 't':
+            flags.t = 1;
+            flags.v = 1;
+            break;
+        case 'T':
+            flags.t = 1;
+            break;
+        }
+        currentFlag = getopt_long(argc, argv, "bevEnstT", LongOptions, NULL);
+    }
+    return flags;
+}
 
-
-void cat_programm(int argc, char *argv) {
+void cat_programm(int argc, char *argv, flags flags)
+{
     int ch;
+    char buffer[SIZE];
     FILE *file = fopen(argv, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("ERROR");
     }
-    else {
-        while ((ch = fgetc(file)) != EOF) {
-            putchar(ch);
+    else if (flags.b)
+    {
+        cat_b(buffer, SIZE, file);
     }
-    fclose(file);
+    else if (flags.n)
+    {
+        cat_n(buffer, SIZE, file);
+    }
+    else if (flags.s)
+    {
+        cat_s(buffer, SIZE, file);
+    }
+    else if (flags.e)
+    {
+        cat_e(file, flags.v);
+    }
+    else if (flags.t)
+    {
+        cat_t(file, flags.v);
+    }
+    else
+    {
+        while ((ch = fgetc(file)) != EOF)
+        {
+            putchar(ch);
+        }
+        fclose(file);
     }
 }
 
-void cat_b(char *buffer, int buffLength, FILE *file) {
-  int counter = 1;
-  while (fgets(buffer, buffLength, file)) {
-    if (buffer[0] != '\n') {
-        printf("%6d%s", counter++, buffer);
+void cat_b(char *buffer, int size, FILE *file)
+{
+    int counter = 1;
+    while (fgets(buffer, size, file))
+    {
+        if (buffer[0] != '\n')
+        {
+            printf("%6d\t", counter++);
+        }
+        printf("%s", buffer);
+    }
+}
+
+void cat_n(char *buffer, int size, FILE *file)
+{
+    int counter = 1;
+    while (fgets(buffer, size, file))
+    {
+        printf("%6d\t%s", counter++, buffer);
+    }
+}
+
+void cat_s(char *buffer, int size, FILE *file)
+{
+    int flag = 0;
+    while (fgets(buffer, size, file))
+    {
+        if (buffer[0] == '\n')
+        {
+            if (flag == 0)
+            {
+                flag = 1;
+                printf("%s", buffer);
+            }
+        }
+        else
+            flag = 0;
+        if (flag == 0)
+        {
+            printf("%s", buffer);
+        }
+    }
+}
+
+
+int transform(int ch) {
+  if (ch == '\n' || ch == '\t')
+    return ch;  // return is allowed in the pre-check
+  if ((ch >= 0 && ch < 9) || (ch > 10 && ch < 32) || (ch > 126 && ch <= 160)) {
+    printf("^");
+    if (ch > 126) {
+      ch -= 64;
+    } else {
+      ch += 64;
     }
   }
+  return ch;
+}
+
+void cat_e(FILE *file, int v) {
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        // Если считанный символ - символ новой строки, выводим "$"
+        if (ch == '\n') {
+            putchar('$');
+        }
+    if (v == 1) {
+        ch = transform(ch);
+    }
+    printf("%c", ch);
+    }
+}
+
+void cat_t(FILE *file, int v) {
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        // Если считанный символ - символ новой строки, выводим "$"
+        if (ch == '\t') {
+            printf("^");
+            ch = '|';
+        }
+    if (v == 1) {
+        ch = transform(ch);
+    }
+    printf("%c", ch);
+    }
 }
--- Cat_try3/s21_cat.h
+++ Cat_try3/s21_cat.h
@@ -3,21 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <getopt.h>
+#include <string.h>
+#define SIZE 256
 
 typedef struct {
-    int b;
-    int e;
-    int n;
-    int s;
-    int t;
-    int v;
+    int b, e, n, s, t, v;
 } flags;
 
-void cat_programm (int argc, char *argv);
+void cat_programm (int argc, char *argv, flags flags);
 void cat_b(char *buffer, int buffLength, FILE *fptr);
-
+void cat_n(char *buffer, int size, FILE *file);
+void cat_s(char *buffer, int size, FILE *file);
+void cat_e(FILE *file, int v);
+void cat_t(FILE *file, int v);
+flags CatReadFlags(int argc, char *argv[]);
+int transform(int ch);
 
 
 #endif
